53f2f0a99ee522b22aa185ca4472692516e95acc
fykyby <95149002+fykyby@users.noreply.github.com>
2023-01-02 17:24:49 +0100

    Add product image carousel

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,6 +4,7 @@ import { useRoute } from "vue-router";
 
 const route = useRoute();
 const product = ref();
+const selectedImg = ref();
 
 async function fetchProduct() {
   const response = await fetch(
@@ -14,6 +15,7 @@ async function fetchProduct() {
   if (data.message) {
     // ERROR
   } else {
+    selectedImg.value = data.images[0];
     product.value = data;
     console.log(data);
   }
@@ -24,22 +26,86 @@ onMounted(fetchProduct);
 
 <template>
   <main>
-    <div class="images">IMG</div>
-    <div class="info">INFO</div>
+    <div class="images" v-if="product">
+      <div class="mainImg">
+        <img :src="selectedImg" alt="Item photo" />
+      </div>
+      <div class="all">
+        <button
+          class="imgBtn"
+          @click="selectedImg = image"
+          :class="{ selected: selectedImg === image }"
+          v-for="image in product.images"
+          :key="image"
+        >
+          <img :src="image" alt="Item photo" />
+        </button>
+      </div>
+    </div>
+    <!-- <div class="info">INFO</div> -->
   </main>
 </template>
 
 <style scoped lang="scss">
 main {
-  border: 1px solid red;
   flex-grow: 1;
   padding: 1rem;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+  display: flex;
+  flex-direction: column;
 
   .images,
   .info {
     display: flex;
+    flex-shrink: 0;
+  }
+
+  .images {
+    overflow-x: hidden;
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+
+    .mainImg {
+      flex-grow: 1;
+      border-radius: var(--border-radius);
+      border: 2px solid var(--color3);
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      aspect-ratio: 1;
+
+      img {
+        height: 100%;
+        object-fit: contain;
+        border-radius: inherit;
+      }
+    }
+
+    .all {
+      display: flex;
+      gap: 0.4rem;
+      overflow-x: auto;
+      flex-shrink: 0;
+
+      .imgBtn {
+        width: 6rem;
+        height: 6rem;
+        flex-shrink: 0;
+        border-radius: var(--border-radius);
+        overflow: hidden;
+        border: 2px solid var(--color4);
+
+        &.selected {
+          border: 2px solid var(--color3);
+        }
+
+        img {
+          height: 100%;
+          width: 100%;
+          object-fit: cover;
+        }
+      }
+    }
   }
 }
 </style>
